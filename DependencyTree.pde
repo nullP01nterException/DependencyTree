6bf9295625307fc0493ef421c2aa077f5413fc8b
nullP01nterException <jolina.lam@sbcglobal.net>
2014-11-21 13:26:08 -0800

    initial push to gitHub. yeaahhhhhh BUDDY\!

---
+++ DependencyTree.pde
@@ -0,0 +1,8 @@
+void setup(){
+size(500,500);
+}
+void draw(){
+	background(0);
+  fill(255,0,0);
+  ellipse(width/2,height/2,50,50);
+}
---
+++ build-tmp/source/DependencyTree.java
@@ -0,0 +1,33 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class DependencyTree extends PApplet {
+
+public void setup(){
+size(500,500);
+}
+public void draw(){
+	background(0);
+  fill(255,0,0);
+  ellipse(width/2,height/2,50,50);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "DependencyTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
